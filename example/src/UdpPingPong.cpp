6cb9fbcf547df214be59fbc60a807457f68f61ff
Dave March <dbmarch@gmail.com>
2020-01-01 17:41:29 -0500

    udp ping pong test is working.

--- example/src/UdpPingPong.cpp
+++ example/src/UdpPingPong.cpp
@@ -22,7 +22,7 @@ public:
     
 
    void ReceiveThread(UdpSocket &udpSocket) {
-      if (mDebug) printf ("PingPongTest Receive Thread started\n");
+      printf ("PingPongTest Receive Thread started\n");
       char buffer[200];
       bool receiving{true};
       while (receiving) {
@@ -32,7 +32,7 @@ public:
 
     			buffer[rval] = 0;
           std::string msg{buffer};
-          printf ("Put %s in MQ\n", msg.c_str());
+          if (mDebug) printf ("Put %s in MQ\n", msg.c_str());
           mMQ.push_back(std::move(msg));
         } else {
             fprintf(stderr, "Receive Error: %s\n", strerror(errno));
@@ -43,7 +43,7 @@ public:
    }
 
  void MainThread (UdpSocket &udpSocket) {
-    if (mDebug) printf ("Udp Test Main Thread  started.\n");
+    printf ("Udp Test Main Thread  started.\n");
     bool waitingReply{false};
     bool running{true};
     int counter{0};
@@ -57,7 +57,10 @@ public:
               printf ("UDP RX: '%s'\n", msg.c_str());
             }
             mMQ.clear();
+            timeoutCounter = 0;
           }
+      }
+      if (timeoutCounter && waitingReply) {
           // We should use std::chrono to timestamp.
           // wait 100msec for now...
           struct timespec tv = {.tv_sec = 0, .tv_nsec = 100000};
@@ -70,6 +73,7 @@ public:
           }
       } else {
       std::string msg = "UDP TX # " + std::to_string(counter);
+      printf ("%s\n", msg.c_str());
       if (udpSocket.SendTo(msg.c_str(), msg.size())< 0) {
         fprintf (stderr,"Udp Failed to send packet %s\n", strerror(errno));
         running = false;
@@ -85,8 +89,8 @@ public:
   }
 
  void RunPingPongTest(UdpSocket &udpSocket) {
-    std::thread main (&PingPongTest::MainThread, this,  std::ref(udpSocket));
     std::thread receive (&PingPongTest::ReceiveThread, this,  std::ref(udpSocket));
+    std::thread main (&PingPongTest::MainThread, this,  std::ref(udpSocket));
     main.join();
     receive.join();
     printf ("ProcessMessages Exiting\n");
@@ -135,7 +139,7 @@ void UdpServerThread (UdpSocket &udpSocket) {
     int rval = udpSocket.RecvFrom(buffer, sizeof(buffer));
     if (rval > 0) {
        buffer[rval] = 0;
-       printf("UDP RX: '%s'\n", buffer);
+       printf("SRVR UDP ECHO: '%s'\n", buffer);
        int status = udpSocket.Reply(buffer, rval);
        if (status < 0) {
          printf ("UDP TX failed: %s\n", strerror(errno));
